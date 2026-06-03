508b7e787de1af4ff55aea98461376df7ba4a1ab
TAMOHARINI R <tamoharini2005@gmail.com>
2026-06-03 14:39:29 +0530

    Add root route for user authentication API

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("User Authentication API is running successfully!");
+});
+
 app.use("/auth", authRouter);
 
 module.exports = app;
